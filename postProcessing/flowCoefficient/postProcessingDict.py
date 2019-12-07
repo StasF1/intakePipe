bee61501bd325e3cd9ebbacfb9b5c3232d16853c
StasF1@macOS <stas.dmitrievich.stashevskiy@gmail.com>
2019-12-07 17:34:23 +0300

    Calculate mass flow rate using python instead of MATLAB

--- postProcessing/flowCoefficient/postProcessingDict.py
+++ postProcessing/flowCoefficient/postProcessingDict.py
@@ -11,25 +11,17 @@ exec(open('../../run/./intakePipeDict').read())
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
 
 # Boundary conditions
-F_inlet = 0.000840365 # square metres, version one # TODO get area from the surfaceFieldValue.dat file
-
 u = [
-    15, # m/s, inlet speed for version one
-    12.1409, # m/s, inlet speed for version two
-    9.9852 # m/s, inlet speed for version three
+    15, # m/s, inlet speed for ver1 case
+    12.1409, # m/s, inlet speed for ver2 case
+    9.9852 # m/s, inlet speed for ver3 case
 ]
 
-# Intake air/gas
-ro_gas = 1.205 # kg/m^3
-
-p_atm = 101325 # Pa, atmospheric pressure
-
-# Pipe geometry
+# Pipe & valve geometry
 d_pipe = 25.688 # mm, the minimum diameter of intake pipe (or diameter for the A point)
 
 d_2Pipe = 29.756 # mm, chamfer diameter of valve neck (or diameter for the A1 point)
 
-# Valve geometry
 d_bar = 6.96 # mm, bar diameter ( d_c )
 
 d_1 = 26.782 # mm, valve diameter ( d_1 )
